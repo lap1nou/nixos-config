b87b8712cae191b3a0310bcdb74f268620b7f92f
lapinou <lapinousexy@gmail.com>
2025-01-31 21:24:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
-# Apply Disko partitioning
-nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko /mnt/etc/nixos/disko-config.nix
-
 # Copy and install NixOS config
 nixos-generate-config --no-filesystems --root /mnt
 cp -R ../nixos-config/. /mnt/etc/nixos
 touch /mnt/etc/nixos/.htb_env
 cd /mnt/etc/nixos/ && git add -f .htb_env
+
+# Apply Disko partitioning
+nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko /mnt/etc/nixos/disko-config.nix
 
 nixos-install --flake /mnt/etc/nixos/.#pentest
