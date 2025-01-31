6485498aeeca00d5a88b8cd2064d3dc209b0f474
lapinou <lapinousexy@gmail.com>
2025-02-01 00:42:35 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -293,11 +293,11 @@ in
   };
 
   # Shared folder VMWare
-  fileSystems."/mount/shared" = {
-    device = ".host:/shared";
-    fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
-    options = ["umask=22" "uid=1000" "gid=100" "allow_other" "defaults" "auto_unmount"];
-  };
+  #fileSystems."/mount/shared" = {
+  #  device = ".host:/shared";
+  #  fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
+  #  options = ["umask=22" "uid=1000" "gid=100" "allow_other" "defaults" "auto_unmount"];
+  #};
 
   imports = [
       ./hardware-configuration.nix
