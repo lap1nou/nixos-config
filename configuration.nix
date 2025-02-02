8a73f67d4bb16246914ed64651f2661ccb18c254
lapinou <lapinousexy@gmail.com>
2025-02-02 19:23:33 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -107,6 +107,7 @@ in
   virtualisation = {
     vmware.guest.enable = true;
     docker.enable = true;
+    vmware.host.enable = true;
 
     # Not enabled because there is no way to check progress
     #oci-containers = {
