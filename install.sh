453e65d8c6cac3bb66e27deb59600f1524f39608
lapinou <lapinousexy@gmail.com>
2025-02-02 19:22:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
+read -s -p "Enter the LUKS password:" password
+echo -n "$password" > /tmp/secret.key
+
 # Install home manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
-
-read -s -p "Enter the LUKS password:" password
-echo -n "$password" > /tmp/secret.key
 
 # Apply Disko partitioning
 nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko ./disko-config.nix
 
 # Copy and install NixOS config
-sleep 3
 nixos-generate-config --no-filesystems --root /mnt
 cp -R ../nixos-config/. /mnt/etc/nixos
 touch /mnt/etc/nixos/.htb_env
