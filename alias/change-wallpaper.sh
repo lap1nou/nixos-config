3b744d327d77b518a2fa4f0527bc587c16c3db2c
lap1nou <lapinousexy@gmail.com>
2025-02-14 20:18:09 +0100

    Added a commentary

--- alias/change-wallpaper.sh
+++ alias/change-wallpaper.sh
@@ -4,6 +4,7 @@ CURRENT_WALLPAPER_PATH="/etc/nixos/pkgs/awesome/themes/1/wallpaper.jpg"
 FZF_COLS=$(($(tput cols) / 2 - 4))
 FZF_LINES=$(tput lines)
 FZF_PREVIEW_SIZE=${FZF_COLS}x${FZF_LINES}@${FZF_COLS}x0
+# Reference: https://github.com/junegunn/fzf/pull/3567
 SELECTED_WALLPAPER=$(find $WALLPAPERS_PATH -type f | fzf --preview="kitty icat --clear --transfer-mod=memory --stdin=no --place=${FZF_PREVIEW_SIZE} {}")
 if [[ ! -z "${SELECTED_WALLPAPER}" ]]; then
     sudo rm $CURRENT_WALLPAPER_PATH
