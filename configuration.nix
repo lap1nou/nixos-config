93baaf75c6f8a9b7c7cb25c8e996f077ee6ff304
lapinou <lapinousexy@gmail.com>
2025-02-23 17:29:57 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -144,6 +144,7 @@ in
   };
 
   nix.extraOptions = ''
+    experimental-features = nix-command flakes
     extra-substituters = https://devenv.cachix.org
     extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
   '';
