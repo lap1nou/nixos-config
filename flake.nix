2d9520a1b2b40e5f0961527259107c2a85696ba8
lapinou <lapinousexy@gmail.com>
2025-02-26 19:48:06 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,11 +6,18 @@
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ nixpkgs, disko, stylix, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, disko, stylix, home-manager, nixos-generators,... }: {
     nixosConfigurations.pentest = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = {
+        testBool = "true";
+      };
       modules = [
         stylix.nixosModules.stylix
         disko.nixosModules.disko
@@ -25,6 +32,27 @@
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
       ];
+    };
+    packages.x86_64-linux = {
+    iso = nixos-generators.nixosGenerate {
+        system = "x86_64-linux";
+        modules = [
+          stylix.nixosModules.stylix
+        disko.nixosModules.disko
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+          {
+            #home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.lapinou = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+        format = "iso";
+        specialArgs = { testBool = "false"; };
+    };
     };
   };
 }
