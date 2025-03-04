a17ef343c8fd0669c36edde3089af34436177638
lapinou <lapinousexy@gmail.com>
2025-03-04 08:55:27 +0100

    Update nix-iso.sh

--- alias/nix-iso.sh
+++ alias/nix-iso.sh
@@ -1,6 +1,4 @@
 git clone https://github.com/lap1nou/nixos-config.git /tmp/nixos-config
-touch /tmp/nixos-config/.htb_env /tmp/nixos-config/.phone-wifi
-cd /tmp/nixos-config/ && git add -f /tmp/nixos-config/.htb_env /tmp/nixos-config/.phone-wifi
 #sudo nixos-generate -f iso --flake /tmp/nixos-config/ -o out.iso
 sudo nix build /tmp/nixos-config/#iso
 rm -rf /tmp/nixos-config
