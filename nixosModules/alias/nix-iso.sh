71c5fc4cae35b5624510ed8e1a7883ccc4b85880
lapinou <lapinousexy@gmail.com>
2025-03-25 23:19:35 +0100

    Update nix-iso.sh

--- nixosModules/alias/nix-iso.sh
+++ nixosModules/alias/nix-iso.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/lap1nou/nixos-config.git /tmp/nixos-config
 #sudo nixos-generate -f iso --flake /tmp/nixos-config/ -o out.iso
-sudo nix build /tmp/nixos-config/#iso
+sudo nix build '/tmp/nixos-config/#iso'
 rm -rf /tmp/nixos-config
