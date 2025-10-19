09ca9e6e9a89012464179e747061c3eb3e4c2ddb
lapinou <lapinousexy@gmail.com>
2025-10-20 00:35:38 +0200

    Export API keys in api_key.sh

--- nixosModules/exegol/my-resources/setup/zsh/api_key.sh
+++ nixosModules/exegol/my-resources/setup/zsh/api_key.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-URLSCAN_API_KEY=
-VIRUSTOTAL_API_KEY=
-WPSCAN_API_TOKEN= # https://github.com/wpscanteam/wpscan?tab=readme-ov-file#load-api-token-from-env-since-v3710
-SHODAN_API_KEY=
-CENSYS_API_KEY=
-DNSDUMPSTER_API_KEY=
-GRAYHATWARFARE_API_KEY=
+export URLSCAN_API_KEY=
+export VIRUSTOTAL_API_KEY=
+export WPSCAN_API_TOKEN= # https://github.com/wpscanteam/wpscan?tab=readme-ov-file#load-api-token-from-env-since-v3710
+export SHODAN_API_KEY=
+export CENSYS_API_KEY=
+export DNSDUMPSTER_API_KEY=
+export GRAYHATWARFARE_API_KEY=
