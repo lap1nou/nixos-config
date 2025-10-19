9398aed8b3ef448615a5d1a96eb75dd4896fd6c1
lapinou <lapinousexy@gmail.com>
2025-10-20 01:48:45 +0200

    Modify yq-go commands for in-place editing
    Updated yq-go commands to use the -i flag for in-place editing.

--- nixosModules/exegol/my-resources/setup/zsh/setup_api_key.sh
+++ nixosModules/exegol/my-resources/setup/zsh/setup_api_key.sh
@@ -1,9 +1,9 @@
 # urlfinder
-yq-go ".urlscan = [${URLSCAN_API_KEY}]" /root/.config/urlfinder/provider-config.yaml
-yq-go ".virustotal = [${VIRUSTOTAL_API_KEY}]" /root/.config/urlfinder/provider-config.yaml
+yq-go -i ".urlscan = [\"${URLSCAN_API_KEY}\"]" /root/.config/urlfinder/provider-config.yaml
+yq-go -i ".virustotal = [\"${VIRUSTOTAL_API_KEY}\"]" /root/.config/urlfinder/provider-config.yaml
 
 # subfinder
-yq-go ".virustotal = [${VIRUSTOTAL_API_KEY}]" /root/.config/subfinder/provider-config.yaml
-yq-go ".shodan = [${SHODAN_API_KEY}]" /root/.config/subfinder/provider-config.yaml
-yq-go ".censys = [${CENSYS_API_KEY}]" /root/.config/subfinder/provider-config.yaml
-yq-go ".dnsdumpster = [${DNSDUMPSTER_API_KEY}]" /root/.config/subfinder/provider-config.yaml
+yq-go -i ".virustotal = [\"${VIRUSTOTAL_API_KEY}\"]" /root/.config/subfinder/provider-config.yaml
+yq-go -i ".shodan = [\"${SHODAN_API_KEY}\"]" /root/.config/subfinder/provider-config.yaml
+yq-go -i ".censys = [\"${CENSYS_API_KEY}\"]" /root/.config/subfinder/provider-config.yaml
+yq-go -i ".dnsdumpster = [\"${DNSDUMPSTER_API_KEY}\"]" /root/.config/subfinder/provider-config.yaml
