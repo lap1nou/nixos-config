263b5b23d8149f2f9955dc422037b5206f657fa5
lapinou <lapinousexy@gmail.com>
2025-10-19 23:22:01 +0200

    Create api_key.sh for managing API keys
    Add a script to store API keys for various services.

---
+++ nixosModules/exegol/my-resources/setup/zsh/api_key.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+URLSCAN_API_KEY=
+VIRUSTOTAL_API_KEY=
+WPSCAN_API_TOKEN= # https://github.com/wpscanteam/wpscan?tab=readme-ov-file#load-api-token-from-env-since-v3710
+SHODAN_API_KEY=
+CENSYS_API_KEY=
+DNSDUMPSTER_API_KEY=
+GRAYHATWARFARE_API_KEY=
