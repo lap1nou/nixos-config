246412a33a511f17783b3839fe7c14976df4b0b3
lapinou <lapinousexy@gmail.com>
2025-12-02 18:35:56 +0100

    Refactor Git configuration settings

--- homeManagerModules/git/git.nix
+++ homeManagerModules/git/git.nix
@@ -12,14 +12,13 @@
   config = lib.mkIf config.homeManagerModules.git.enable {
     programs.git = {
       enable = true;
-      userEmail = "lapinousexy@gmail.com";
-      userName = "lap1nou";
-      signing.signByDefault = true;
-
-      extraConfig = {
+      settings = {
+        user.email = "lapinousexy@gmail.com";
+        user.name = "lap1nou";
         gpg.format = "ssh";
         user.signingkey = "~/.ssh/id_rsa_github.pub";
       };
+      signing.signByDefault = true;
     };
   };
 }
